64c29f0437ee49cb2d667ef231e2e3eaae277eb9
SathtikBose <sathtikbose@gmail.com>
2025-11-26 23:56:14 +0530

    updated index files

--- index.js
+++ index.js
@@ -2,12 +2,18 @@ import express from 'express';
 import fs from 'fs';
 import csv from 'csv-parser';
 import cors from 'cors';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 const PORT = 3000;
 
 app.use(cors());
-app.use(express.static('public'));
+app.use(cors());
+app.use(express.static(path.join(__dirname, 'public')));
 
 const movies = [];
 
